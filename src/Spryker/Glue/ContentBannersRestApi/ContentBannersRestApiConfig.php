23b1c4cae2b20c56ec85227eb9735919cdb801d4
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/ContentBannersRestApi/ContentBannersRestApiConfig.php
+++ src/Spryker/Glue/ContentBannersRestApi/ContentBannersRestApiConfig.php
@@ -12,41 +12,57 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class ContentBannersRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CONTENT_BANNERS = 'content-banners';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_CONTENT_BANNERS = 'content-banners-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONTENT_NOT_FOUND = '2201';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CONTENT_NOT_FOUND = 'Content not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONTENT_KEY_IS_MISSING = '2202';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CONTENT_KEY_IS_MISSING = 'Content key is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONTENT_TYPE_INVALID = '2203';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CONTENT_TYPE_INVALID = 'Content type is invalid.';
